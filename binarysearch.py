b8742926a7cd44217c3076b1180aeeac105491f3
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-04-09 16:14:51 -0700

    adding binary search impl on sorted list

---
+++ binarysearch.py
@@ -0,0 +1,26 @@
+from typing import List
+
+"""
+Binary search implementation on a sorted array to find target integer,
+O(n log n) time complexity
+"""
+
+
+def binary_search(nums:List[int], target:int) -> int:
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        mid = (l + r)//2
+        if nums[mid] > target:
+            r = mid - 1
+        elif nums[mid] < target:
+            l = mid + 1
+        else:
+            return mid
+    return 0
+
+
+if __name__ == "__main__":
+    nums = [-1, 0, 3, 5, 9, 12]
+    target = 9
+    print(binary_search(nums, target))
