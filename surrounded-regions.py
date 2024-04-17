b9c702add582f328ea963370e38b36bc95f5235f
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-04-17 09:05:24 -0700

    upd surrounded-regions

--- surrounded-regions.py
+++ surrounded-regions.py
@@ -4,52 +4,51 @@ from typing import List
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         if not board:
-            return
-
+            return None
+            
         rows, cols = len(board), len(board[0])
         visit = set()
-
         def bfs(r, c):
-            queue = collections.deque()
+            queue = deque()
+            component = [(r, c)]  #holds all the cells of a connected group of 'O's that are reached during the BFS from a starting cell. Initialize it with current cell
             queue.append((r, c))
-            visit.add((r, c))
-            component = []
-            component.append((r, c))
-            is_boundary_connected = r == 0 or c == 0 or r == rows - 1 or c == cols - 1 #flag which becomes True if any cells are on boundary
-            directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+            visit.add((r, c))  # Mark starting cell as visited
 
+            # flag to check if cell is within the boundary of grid
+            is_boundary = r == 0 or c == 0 or r == rows - 1 or c == cols - 1
+            directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
             while queue:
-                nr, nc = queue.popleft()
+                qr, qc = queue.popleft()
                 for dr, dc in directions:
-                    rr, cc = nr + dr, nc + dc
-                    if 0 <= rr < rows and 0 <= cc < cols and board[rr][cc] == 'O' and ((rr, cc)) not in visit:
-                        visit.add((rr, cc))
-                        queue.append((rr, cc))
-                        component.append((rr, cc))
-                        if rr == 0 or cc == 0 or rr == rows - 1 or cc == cols - 1:
-                            is_boundary_connected = True
-
-            if not is_boundary_connected:
+                    nr, nc = qr + dr, qc + dc
+                    if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] == "O" and (nr, nc) not in visit:
+                        # mark neighbor as visited and add to the queue and component list
+                        visit.add((nr, nc))
+                        queue.append((nr, nc))
+                        component.append((nr, nc))
+                        # upd boundary status if the neighbor is on the boundary
+                        if nr == 0 or nc == 0 or nr == rows - 1 or nc == cols - 1:
+                            is_boundary = True
+            
+            #if the component is not connected to any boundary, mark all its cells as 'X'.
+            if not is_boundary:
                 for row, col in component:
-                    board[row][col] = 'X'
+                    board[row][col] = "X"
 
+        # start BFS from all 'O's on the vertical border
         for r in range(rows):
             for c in [0, cols-1]:
-                if board[r][c] == 'O' and (r, c) not in visit:
+                if board[r][c] == "O" and (r, c) not in visit:
                     bfs(r, c)
+
+        # start BFS from all 'O's on the horizontal border
         for c in range(cols):
             for r in [0, rows-1]:
-                if board[r][c] == 'O' and (r, c) not in visit:
+                if board[r][c] == "O" and (r, c) not in visit:
                     bfs(r, c)
 
+        # mark all other unvisited 'O's to 'X'.
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == 'O' and (r, c) not in visit:
-                    board[r][c] = 'X'
-
-
-
-
-if __name__ == "__main__":
-  pass
-  
+                if board[r][c] == "O" and (r, c) not in visit:
+                    board[r][c] = "X"
