301f01d8499853f12ae99ff54a52652c4f694805
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-04-20 13:12:17 -0700

    add leetcode 235 - lowest common ancestor

---
+++ LCA.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None 
+
+        cur = root
+
+        while cur:
+            #if both nodes p and q are greater than current, move to the right subtree.
+            if p.val > cur.val and q.val > cur.val:
+                cur = cur.right #update cur to root of right subtree
+            #if both nodes p and q are less than current, move to left subtree
+            elif p.val < cur.val and q.val < cur.val:
+                cur = cur.left #update cur to root of left subtree
+            else:
+                return cur  #current node is the LCA of p and q
+                
+        
