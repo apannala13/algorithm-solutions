f25faa62f106d8b2abad3cce6f6768a28bb3afa3
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-04-27 10:50:14 -0700

    add dfs impl

--- courseschedule2.py
+++ courseschedule2.py
@@ -27,10 +27,6 @@ class Solution:
 
 
 #dfs
-
-from collections import defaultdict
-from typing import List
-
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
