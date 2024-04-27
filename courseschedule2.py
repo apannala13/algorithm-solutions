7b8a2131194b564b70ff3181f9329ba1f2e3627c
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-04-27 10:49:58 -0700

    add dfs impl

--- courseschedule2.py
+++ courseschedule2.py
@@ -22,5 +22,47 @@ class Solution:
                     queue.append(neighbor)
         
         return top_order if len(top_order) == numCourses else []
-                
+
+
+
+
+#dfs
+
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        #states: 0 = unvisited, 1 = visiting, 2 = visited
+        visit = [0] * numCourses
+        top_order = []
+        
+        for crs, pre in prerequisites:
+            graph[pre].append(crs)
+            
+        def dfs(crs):
+            if visit[crs] == 1:  #cycle detected
+                return False
+            if visit[crs] == 2:  #already processed
+                return True
+            
+            visit[crs] = 1  #mrk the course as visiting for cycle detection
+            for next_course in graph[crs]:
+                if not dfs(next_course):
+                    return False
+            visit[crs] = 2  #mark the course as visited
+            top_order.append(crs)  #add the course to the topological order
+            return True
+        
+        #visit each course
+        for crs in range(numCourses):
+            if visit[crs] == 0:  # Unvisited
+                if not dfs(crs):
+                    return []  #if a cycle is detected, return an empty list
+        
+        # courses were added in reverse topological order, so returning inverse
+        return top_order[::-1]  
+
+
       
