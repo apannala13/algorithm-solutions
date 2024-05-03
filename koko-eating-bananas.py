36cdec246e7fb96386b9c7a8774d7009b91673bd
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-05-03 11:47:07 -0700

    add lc 875

---
+++ koko-eating-bananas.py
@@ -0,0 +1,28 @@
+import math 
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        #O(log (max(p)) * p)
+
+        l, r = 1, max(piles)
+
+        #maximum value in piles, which is the highest possible speed
+        res = max(piles)
+
+        while l <= r:
+            #midpoint 'k', the current candidate for the minimum speed
+            k = (l + r)//2
+            #track total hours needed with the current speed 'k'
+            hours = 0
+            #calculate the hours needed to eat the pile at speed 'k'
+            #e.g.: 3/2 + 6/2 + 7/2 + 11/2 should be <= 8
+            for pile in piles:
+                hours += math.ceil((pile/k)) #round up since partial hours count as full hours
+            #If the total hours with speed 'k' are less than or equal to 'h'
+            if hours <= h:
+                res = min(res, k) #update 'res' to the smaller of the current 'res' or 'k'
+                r = k - 1 #decrease 'r' to narrow the search bounds, ignoring speeds higher than 'k'
+            else:
+                l = k + 1 #increase 'l' to narrow the search bounds, ignoring speeds lower than 'k'
+        return res #smallest speed found that allows all bananas to be eaten within 'h' hours
+        
